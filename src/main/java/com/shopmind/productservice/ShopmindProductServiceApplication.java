ad81928eebf6272ee06ea440e2b360a1316957f5
hcy180 <hcy1805110063@163.com>
2025-12-21 14:41:32 +0800

    基础项目搭建

---
+++ src/main/java/com/shopmind/productservice/ShopmindProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopmind.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmindProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopmindProductServiceApplication.class, args);
+    }
+
+}
