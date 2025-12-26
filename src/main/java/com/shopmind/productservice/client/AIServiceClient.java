77a23a21e6722f5e2e855f92f23f71b5598ea045
hcy180 <hcy1805110063@163.com>
2025-12-26 19:31:29 +0800

    RPC 搭建

---
+++ src/main/java/com/shopmind/productservice/client/AIServiceClient.java
@@ -0,0 +1,13 @@
+package com.shopmind.productservice.client;
+
+import org.springframework.web.service.annotation.HttpExchange;
+
+/**
+ * Description:
+ * Author: huangcy
+ * Date: 2025-12-26
+ */
+@HttpExchange("/ai")
+public class AIServiceClient {
+
+}
---
+++ src/main/java/com/shopmind/productservice/config/HttpExchangeConfig.java
@@ -0,0 +1,25 @@
+package com.shopmind.productservice.config;
+
+import com.shopmind.framework.constant.ServiceNameConstant;
+import com.shopmind.framework.util.ShopmindHttpClientUtils;
+import com.shopmind.productservice.client.AIServiceClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+/**
+ * Description: RPC 服务调用配置类
+ * Author: huangcy
+ * Date: 2025-12-26
+ */
+@Configuration
+public class HttpExchangeConfig {
+
+    /**
+     * 远程 shopmind-ai-service 服务（框架已负载均衡）
+     */
+    @Bean
+    public AIServiceClient  aiServiceClient(RestClient.Builder builder) {
+        return ShopmindHttpClientUtils.createLoadBalancedClient(builder, ServiceNameConstant.AI_SERVICE, AIServiceClient.class);
+    }
+}
