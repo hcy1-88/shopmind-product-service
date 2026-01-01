25101ef701acc2fffbf5b1f6200a98f7782febc8
hcy180 <hcy1805110063@163.com>
2026-01-02 02:52:35 +0800

    分页插件

---
+++ src/main/java/com/shopmind/productservice/config/MybatisPlusConfig.java
@@ -0,0 +1,19 @@
+package com.shopmind.productservice.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 添加分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL)); // 根据你的数据库选类型
+        return interceptor;
+    }
+}
